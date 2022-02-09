10233fbd36406b6d40d62270662d4669d28cbcf6
lynnkays <95385645+lynnkays@users.noreply.github.com>
2022-02-09 17:46:13 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,57 @@
+import smtplib, ssl
+
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import time
+
+COMMASPACE = ', '
+
+subject = "An email with attachment from Python"
+body = "This is an email with attachment sent from Python"
+sender_email = ""
+password = ""
+
+# Create a multipart message and set headers
+message = MIMEMultipart()
+message["From"] = sender_email
+message["To"] = ""
+
+# Add body to email
+message.attach(MIMEText(body, "plain"))
+
+filename = "something2.txt"  # In same directory as script
+
+# Open PDF file in binary mode
+with open(filename, "rb") as attachment:
+    # Add file as application/octet-stream
+    # Email client can usually download this automatically as attachment
+    part = MIMEBase("application", "octet-stream")
+    part.set_payload(attachment.read())
+
+# Encode file in ASCII characters to send by email
+encoders.encode_base64(part)
+
+# Add header as key/value pair to attachment part
+part.add_header(
+    "Content-Disposition",
+    f"attachment; filename= {filename}",
+)
+
+# Add attachment to message and convert message to string
+message.attach(part)
+
+for x in range(2500, 3000):
+    message['Subject'] = f'Test #: {x}'
+    text = message.as_string()
+
+
+    # Log in to server using secure context and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, message["To"].split(","), text)
+
+    time.sleep(3)
+    print(f'Email #: {x} sent!')
